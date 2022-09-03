aebe0c479a7ca892dfac78ec3209dca51876774b
HarryPotter0308 <creativemoderndev0831@gmail.com>
2022-09-03 10:50:29 +0900

    message Update

--- public/assets/js/src/components/navbar.js
+++ public/assets/js/src/components/navbar.js
@@ -1,7 +1,5 @@
 document.getElementById("search-checkbox").addEventListener("change", function (e) {
-    console.log("a");
     if (e.target.checked) {
-        console.log("b");
         setTimeout(function () {
             const input = document.getElementById("nav-search-input");
             input.focus();
